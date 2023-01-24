5eb2ca71bfe8ca98a40a1786cdf39a961cf451a4
Ahindra D <83480142+AhindraD@users.noreply.github.com>
2023-01-24 17:08:40 +0530

    Update ApiConfig.js

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://docseek-db.up.railway.app/",//"http://localhost:8000/",//"https://docseek-db.up.railway.app/",
+    baseURL: "https://docseek.onrender.com/",//"http://localhost:8000/",//"https://docseek-db.up.railway.app/",
 })
 
 axiosClient.interceptors.request.use(
